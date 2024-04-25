f28611a4fc3c44fce689b4549f2a4108bc9f435f
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-25 17:13:20 +0700

    void traverse

--- 050_singlelinkedlist.cpp
+++ 050_singlelinkedlist.cpp
@@ -79,7 +79,7 @@ bool deleteNode(int nim) {
 		return false;
 	previous->next = current->next;
 	if (current == START)
-		START current->next;
+		START = current->next;
 	return true;
 }
 bool listEmpty() {
@@ -89,3 +89,13 @@ bool listEmpty() {
 		return false;
 
 }
+
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	
+
