86d4269e1f1b28b01e8e460b275a2f3a39e92a99
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-25 16:53:37 +0700

    start->noMhs

--- 050_singlelinkedlist.cpp
+++ 050_singlelinkedlist.cpp
@@ -12,11 +12,18 @@ Node* START = NULL;
 void addNode() {
 	int nim;
 	string nama;
-	Node* nodeBaru
-		new Node();
+	Node* nodeBaru = new Node();
 	cout << "Masukkan NIM: ";
 	cin >> nim;
 	cout << "Masukkan Nama: ";
 	cin >> nama;
 	nodeBaru->noMhs = nim;
 	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		
