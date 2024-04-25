72af7432e1eae000c6e281bb594d04a34c1721df
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-25 17:28:10 +0700

    case 2

--- 050_singlelinkedlist.cpp
+++ 050_singlelinkedlist.cpp
@@ -150,4 +150,12 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
-			
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+				int nim;
