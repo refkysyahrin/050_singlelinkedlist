8eefb637fc4fb98c5438d2dfc6f312d3bb862ecc
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-04-25 17:27:35 +0700

    int main try

--- 050_singlelinkedlist.cpp
+++ 050_singlelinkedlist.cpp
@@ -130,3 +130,18 @@ void searchData() {
 	}
 }
 
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl; cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
+			
